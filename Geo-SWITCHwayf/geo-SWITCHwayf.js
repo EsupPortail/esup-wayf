121996721865ce3ed1f0b0e77db046bfbe0fd6c8
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2026-09-22 12:26:10 +0200

    fix: replace cartodb-basemaps with "standard" OSM (raster tiles)

--- Geo-SWITCHwayf/geo-SWITCHwayf.js
+++ Geo-SWITCHwayf/geo-SWITCHwayf.js
@@ -42,7 +42,7 @@ function toggleCheckbox(element){
 $(function(){
 
 	function initMap(){
-		L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; Données cartographiques <a href="http://osm.org/copyright">OpenStreetMap</a>'
 		}).addTo(map);
 	};
